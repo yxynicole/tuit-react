6ef0643cd48f1ee494d6e40013586ed98f70149b
yxynicole <ye.xinyu@northeastern.edu>
2022-03-19 22:14:07 -0400

    change URL

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const TUITS_API = "https://cs5500-01-sp22.herokuapp.com/api/tuits";
-const USERS_API = "https://cs5500-01-sp22.herokuapp.com/api/users";
+const TUITS_API = "https://powerful-citadel-40623.herokuapp.com/api/tuits";
+const USERS_API = "https://powerful-citadel-40623.herokuapp.com/api/users";
 
 export const findAllTuits = () =>
     axios.get(TUITS_API)
